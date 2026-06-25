1a10fb0f66b93b853ccb2207fbf651bb9075c28b
Satyam <justsaysatyam@gmail.com>
2026-06-25 14:15:57 +0530

    Add Render deployment configuration and update allowed hosts

--- school_management/settings.py
+++ school_management/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-(5w%d1g^st1z2^#ov6dzm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.railway.app, midpointschool.online,www.midpointschool.online').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.railway.app,.onrender.com,midpointschool.online,www.midpointschool.online').split(',')
 
 # Application definition
 INSTALLED_APPS = [
@@ -167,4 +167,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_TRUSTED_ORIGINS = [
     "https://midpointschool.up.railway.app",
     "https://midpointschool.online",
+    "https://*.onrender.com",
 ]
